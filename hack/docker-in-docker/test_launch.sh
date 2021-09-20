aadc03929469b29912e7cca4f7012e1df9aa8f52
florencepascual <86305580+florencepascual@users.noreply.github.com>
2021-09-20 14:42:34 +0200

    Create test_launch.sh

---
+++ hack/docker-in-docker/test_launch.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# script that launches tests from the repo https://github.ibm.com/powercloud/dockertest
+
+set -ue
+
+sh ${PATH_SCRIPTS}/dockerd-entrypoint.sh &
+source ${PATH_SCRIPTS}/dockerd-starting.sh
+
+if ! test -d /root/.docker
+then
+    mkdir /root/.docker
+    echo "${SECRET_AUTH}" > /root/.docker/config.json
+fi
+
+echo "= Docker test suite for ${DISTRO_NAME} ="
+export GOPATH=${WORKSPACE}/test:/go
+export GO111MODULE=auto
+cd /workspace/test/src/github.ibm.com/powercloud/dockertest
+make test WHAT="./tests/${DISTRO_NAME}" GOFLAGS="-v"
+
+echo "== End of the docker test suite =="
+
+exit 0
